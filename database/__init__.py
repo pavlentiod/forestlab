65607c5768c8b84c01792af575765835e7222bd9
pavlentiod <levap.spb@gmail.com>
2024-09-07 12:44:28 +0300

    Remove Base class for tables and db_helper

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,3 @@
-__all__ = {
-    "db_helper",
-    "Event",
-    "Base"
-}
-
-from src.database.base import Base
-from src.database.db_helper import db_helper
 from src.database.models.event.event import Event
+
+__all__ = [Event]
