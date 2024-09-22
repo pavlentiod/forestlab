e23c8d620a280711a21f68160416eb9d689e8f67
pavlentiod <levap.spb@gmail.com>
2024-09-22 23:18:59 +0300

    Move

--- services/event/subservices/parser/src/punching_systems/utils.py
+++ services/event/subservices/parser/src/punching_systems/utils.py
@@ -3,15 +3,9 @@ import re
 from typing import Union
 
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup as BS, BeautifulSoup
-from fastapi import UploadFile
-from pandas import Timedelta, DataFrame
+from bs4 import BeautifulSoup as BS
+from pandas import Timedelta
 from pandas._libs import NaTType
-
-from src.services.event.parser.src.punching_systems.SFR_parse import SFR_parsing
-from src.services.event.parser.src.punching_systems.SPORT_ORG_parse import SO_parsing
-from src.services.event.parser.src.punching_systems.WinOrient_parse import SI_parsing
 
 
 def bs(s: BS):
@@ -62,36 +56,3 @@ def routes_and_splits_dict(routes: list, splits: list):
 def dispersions(d: dict):
     disp = {k: {leg: n for n, leg in enumerate(v)} for k, v in d.items()}
     return pd.DataFrame(disp).T
-
-
-def get_source(source_link: str, source_file: UploadFile) -> BeautifulSoup:
-    if source_link:
-        return web_parse(source_link)
-    else:
-        return file_parse(source_file)
-
-
-def parse_event(page: BeautifulSoup) -> (DataFrame, DataFrame):
-    title = str(page.find('title'))
-    if 'WinOrient' in title:
-        return SI_parsing(page)
-    elif 'SportOrg' in title:
-        return SO_parsing(page)
-    else:
-        return SFR_parsing(page)
-
-
-def web_parse(link: str) -> BeautifulSoup:
-    content = ''
-    if 'http' in link:
-        response = requests.get(link)
-        try:
-            content = response.content.decode(response.apparent_encoding)
-        except UnicodeEncodeError as e:
-            content = ''
-    soup_page = bs(content)
-    return soup_page
-
-
-def file_parse(data: bytes) -> BeautifulSoup:
-    pass
