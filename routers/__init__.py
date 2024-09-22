eb2497a41764fa204b837a6ea669d91451c40485
pavlentiod <levap.spb@gmail.com>
2024-09-22 23:03:00 +0300

    Add track router

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from .group.group_router import router as group_router
-from .event.event_router import router as event_router
-from .runner.runner_router import router as runner_router
+
+from src.routers.events.events_router import event_router
+from src.routers.single_event_data.single_event_data_router import event_data_router
+from src.routers.single_event_statistics.single_event_statistics_router import event_statistics_router
+from src.routers.track.track_router import track_router
+
 router = APIRouter()
-router.include_router(group_router, prefix='/group', tags=['Group'])
-router.include_router(event_router, prefix='/event', tags=['Event'])
-router.include_router(runner_router, prefix='/runner', tags=['Runner'])
+router.include_router(event_router, prefix='/events', tags=['Event'])
+router.include_router(event_data_router, prefix='/events', tags=['Events'])
+router.include_router(event_statistics_router, prefix='/events', tags=['Event statistics'])
+router.include_router(track_router, prefix='/events', tags=['Tracks'])
+
+__all__ = [router]
