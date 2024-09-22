9f60201adb7741b46e547a892782138a87ab44f3
pavlentiod <levap.spb@gmail.com>
2024-09-22 23:13:49 +0300

    Edit main app file

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-import datetime
+from fastapi import FastAPI
 
-import pandas as pd
+# Initialize FastAPI application
+from src.routers import router
 
-from src.database import db_helper
+app = FastAPI(
+    title="Event Data API",
+    description="API for managing and retrieving event data",
+    version="1.0.0",
+    docs_url="/docs",  # URL for the Swagger UI documentation
+    redoc_url="/redoc",  # URL for ReDoc documentation
+)
 
+# Connect the router to the FastAPI app
+app.include_router(
+    router=router
+)
 
-async def main():
-    async with db_helper.session_factory() as session:
-        print(pd.read_csv('c.csv').memory_usage().sum())
-        # event_service = EventService(session)
-        # request = EventEndpoint(title='ZZ',
-        #                         source="https://o-site.spb.ru/_races/240901ZZ/240901zz_split.htm",
-        #                         date=datetime.datetime.now().date())
-        # ev = await event_service.create(request)
+# Define a root route for health check or basic information
+@app.get("/", tags=["Root"])
+async def read_root():
+    """
+    Root endpoint for basic health check or welcome message.
+    """
+    return {"message": "Welcome to the Event Data API"}
 
-
-
-
-
-
+# Run the app using Uvicorn if the script is executed directly
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
-
+    import uvicorn
+    uvicorn.run("main:app", port=8005, reload=False)
